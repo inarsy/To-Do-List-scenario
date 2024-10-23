86c87219b96ef5c91d0e7801c3b3cfced4c80a40
NoahJDRobinson <nrobinson7820@gmail.com>
2024-10-23 10:49:53 +0100

    removed semi colons

--- index.php
+++ index.php
@@ -1,59 +1,59 @@
 <?php
 session_start();
 
-echo"<!DOCTYPE html>;
+echo"<!DOCTYPE html>
 
-<html lang='en'>;
+<html lang='en'>
 
-<head>;
+<head>
 
-<link rel='stylesheet' href='styles.css'>;
-<title> Watkin's ToDo List website</title>;
+<link rel='stylesheet' href='styles.css'>
+<title> Watkin's ToDo List website</title>
 
-</head>;
+</head>
 
-<body>;
+<body>
 
-<div id='container'>;
+<div id='container'>
 
-<div id='title'>;
+<div id='title'>
 
-<h3 id='banner'>Watkin's ToDo List</h3>;
+<h3 id='banner'>Watkin's ToDo List</h3>
 
-</div>;
+</div>
 
-<div id='navbar'>;
+<div id='navbar'>
 
-<ul id='menu'>;
+<ul id='menu'>
 
-<a href='index.php'><li> Home </li></a>";
+<a href='index.php'><li> Home </li></a>"
 
 if (empty($_SESSION["ssnlogin"])) {
-    echo "<a href='login.php'><li> Login </li></a>; 
+    echo "<a href='login.php'><li> Login </li></a> 
           <a href='register.php'><li> Register </li></a>";
 }
 
 elseif ($_SESSION["ssnlogin"]) {
-    echo "<a href='profile.php' <li> Profile </li></a>;
-        <a href='list.php'><li> Lists </li></a>;
+    echo "<a href='profile.php' <li> Profile </li></a>
+        <a href='list.php'><li> Lists </li></a>
         <a href='logout.php'<li> Logout </li></a>";
 }
 
-echo "</ul>;
+echo "</ul>
 
-</div>;
+</div>
 
-<div id='content'>;
+<div id='content'>
 
-<p>Welcome to the Watkin's ToDo list website. Here you can sign up to create and manage all of your to do lists in one easy place</p>;
+<p>Welcome to the Watkin's ToDo list website. Here you can sign up to create and manage all of your to do lists in one easy place</p>
 
-<p>You will need to register first to be able to make use of the system, but after that its all free to use.</p>;
+<p>You will need to register first to be able to make use of the system, but after that its all free to use.</p>
 
-</div>;
+</div>
 
-</div>;
+</div>
 
-</body>;
+</body>
 
 </html>";
 
